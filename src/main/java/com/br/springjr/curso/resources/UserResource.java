638502fb5bdd7d146372bd97eb75aab44f83a3b0
Vagner Júnior <valbpjr@gmail.com>
2025-09-10 15:36:00 -0300

    commit resources

---
+++ src/main/java/com/br/springjr/curso/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.br.springjr.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.br.springjr.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "junior","junior@gmail","84562256", "123456789");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
