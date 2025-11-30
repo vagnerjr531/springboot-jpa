eef92511d239213e22ef52b8baf07e862a69a3b3
Vagner Júnior <valbpjr@gmail.com>
2025-11-30 19:53:09 -0300

    ExcptionHandling Update

--- src/main/java/com/br/springjr/curso/services/UserService.java
+++ src/main/java/com/br/springjr/curso/services/UserService.java
@@ -13,6 +13,8 @@ import com.br.springjr.curso.repository.UserRepository;
 import com.br.springjr.curso.services.exceptions.DataBaseException;
 import com.br.springjr.curso.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 
 @Service
 public class UserService {
@@ -42,9 +44,15 @@ public class UserService {
 		}
 	}
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
+	
 		return repository.saveAndFlush(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+			
+		}
 	}
 
 	private void updateData(User entity, User obj) {
